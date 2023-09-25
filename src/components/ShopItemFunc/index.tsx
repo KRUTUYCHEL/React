bd6e45a2823af37edc48dc5de11d363eb5eadf62
Vladimir <wov4@rambler.ru>
2023-09-25 16:49:07 +0200

    Задание завершино

--- src/components/ShopItemFunc/index.tsx
+++ src/components/ShopItemFunc/index.tsx
@@ -13,18 +13,18 @@ const ShopItemFunc = ({brand, description, descriptionFull, price, title, curren
   return (
     <>
       <div className="main-content">
-        <h2>Tiger of Sweden</h2>
-        <h1>Leonard coat</h1>
-        <h3>Minimalistic coat in cotton-blend</h3>
+        <h2>{brand}</h2>
+        <h1>{title}</h1>
+        <h3>{description}</h3>
         <div className="description">
-          {description}
+          {descriptionFull}
         </div>
         <div className="highlight-window mobile">
-          <div className="highlight-overlay"></div>
+          <div className="highlight-overlay" />
         </div>
-        <div className="divider"></div>
+        <div className="divider"/>
         <div className="purchase-info">
-          <div className="price">{currency}{price}</div>
+          <div className="price">{currency}{price.toFixed(2)}</div>
           <button>Добавить в корзину</button>
         </div>
       </div>
